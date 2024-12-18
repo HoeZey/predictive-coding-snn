088dec5e1f959822d8b8a30e081a888c652f8141
Hoshe Lee <hoshelee@rewirenow.com>
2024-12-18 11:48:39 +0100

    Training non bottleneck ssl model

--- scripts/train/train_model_ssl_NE.sh
+++ scripts/train/train_model_ssl_NE.sh
@@ -2,11 +2,11 @@
 
 #SBATCH --partition=gpu
 #SBATCH --gpus=1
-#SBATCH --job-name=TR_B_NE
+#SBATCH --job-name=TR_S_NE
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --time=1:00:00
-#SBATCH --output=outputs/train_model/bottleneck_NE/output_%A.out
+#SBATCH --output=outputs/train_model/ssl_NE/output_%A.out
 
 module purge
 module load 2023
@@ -15,5 +15,5 @@ module load Anaconda3/2023.07-2
 cd $HOME/nddl/
 
 source activate nddl
-srun python main_train_ssl.py -f configs/train_bottleneck_no_energy.toml
+srun python main_train_ssl.py -f configs/train_ssl_no_energy.toml
 conda deactivate
